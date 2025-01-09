ce3829f51a844c6bec123c70566939442dbb54d2
kkkkendei <authoryu0607@gmail.com>
2025-01-09 18:14:38 +0800

    封装Jwt签名

--- api-gateway-core/src/main/java/com/wuzeyu/gateway/authorization/IAuth.java
+++ api-gateway-core/src/main/java/com/wuzeyu/gateway/authorization/IAuth.java
@@ -1,4 +1,13 @@
 package com.wuzeyu.gateway.authorization;
 
-public class IAuth {
+
+/**
+ * @author wuzeyu
+ * @description 认证服务接口
+ * @github github.com/kkkkendei
+ */
+public interface IAuth {
+
+    boolean validate(String id, String token);
+
 }
