5317a8cd9356a724acb9a1bd3a7bae64436729bd
kkkkendei <authoryu0607@gmail.com>
2025-01-09 22:14:12 +0800

    shiro

--- api-gateway-core/src/main/java/com/wuzeyu/gateway/authorization/GatewayAuthorizingRealm.java
+++ api-gateway-core/src/main/java/com/wuzeyu/gateway/authorization/GatewayAuthorizingRealm.java
@@ -1,4 +1,43 @@
 package com.wuzeyu.gateway.authorization;
 
-public class GatewayAuthorizingRealm {
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author wuzeyu
+ * @description 验证领域
+ * @github github.com/kkkkendei
+ */
+public class GatewayAuthorizingRealm extends AuthorizingRealm {
+
+    @Override
+    public Class<?> getAuthenticationTokenClass() {
+        return GatewayAuthorizingToken.class;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        // 暂不做授权处理
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        try {
+            // 验证解析是否报错
+            JwtUtil.decode(((GatewayAuthorizingToken) token).getJwt());
+        } catch (Exception e) {
+            throw new AuthenticationException("无效令牌");
+        }
+
+        return new SimpleAuthenticationInfo(token.getPrincipal(), token.getCredentials(), this.getName());
+
+    }
+
 }
--- api-gateway-core/src/main/java/com/wuzeyu/gateway/authorization/GatewayAuthorizingToken.java
+++ api-gateway-core/src/main/java/com/wuzeyu/gateway/authorization/GatewayAuthorizingToken.java
@@ -1,4 +1,40 @@
 package com.wuzeyu.gateway.authorization;
 
-public class GatewayAuthorizingToken {
+import org.apache.shiro.authc.AuthenticationToken;
+
+/**
+ * @author wuzeyu
+ * @description 验证Token
+ * @github github.com/kkkkendei
+ */
+public class GatewayAuthorizingToken implements AuthenticationToken {
+
+    private static final long serialVersionUID = 1L;
+
+    // 通信管道ID
+    private String channelId;
+
+    // JSON WEB TOKEN
+    private String jwt;
+
+    public GatewayAuthorizingToken() {}
+
+    public GatewayAuthorizingToken(String channelId, String jwt) {
+        this.channelId = channelId;
+        this.jwt = jwt;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return channelId;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return this.jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
 }
