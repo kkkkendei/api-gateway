88bb2e5b62462d128153167337448df0fdf6a148
kkkkendei <authoryu0607@gmail.com>
2025-01-15 23:51:13 +0800

    test: 测试注册网关服务节点通过

--- api-gateway-center/src/test/java/com/wuzeyu/gateway/center/test/ApiTest.java
+++ api-gateway-center/src/test/java/com/wuzeyu/gateway/center/test/ApiTest.java
@@ -4,6 +4,7 @@ package com.wuzeyu.gateway.center.test;
 import com.alibaba.fastjson.JSON;
 import com.wuzeyu.gateway.center.application.IConfigManageService;
 import com.wuzeyu.gateway.center.domain.manage.model.ApiData;
+import com.wuzeyu.gateway.center.domain.manage.model.vo.GatewayServerVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -23,15 +24,23 @@ import java.util.List;
 @SpringBootTest
 public class ApiTest {
 
-    private Logger LOG = LoggerFactory.getLogger(ApiTest.class);
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
 
     @Resource
-    private IConfigManageService apiService;
+    private IConfigManageService configManageService;
 
     @Test
-    public void test() {
-        List<ApiData> apiDataList = apiService.queryHttpStatementList();
-        LOG.info("测试结果：{}", JSON.toJSONString(apiDataList));
+    public void test_queryGatewayServerList() {
+        List<GatewayServerVO> gatewayServerVOS = configManageService.queryGatewayServerList();
+        logger.info("测试结果：{}", JSON.toJSONString(gatewayServerVOS));
+    }
+
+    @Test
+    public void test_registerGatewayServerNode() {
+        configManageService.registerGatewayServerNode("10001", "api-gateway-g1", "电商支付网关", "127.0.0.196");
+        configManageService.registerGatewayServerNode("10001", "api-gateway-g2", "电商支付网关", "127.0.0.197");
+        configManageService.registerGatewayServerNode("10001", "api-gateway-g3", "电商配送网关", "127.0.0.198");
     }
 
 }
+
