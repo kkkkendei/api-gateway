11e80b44157557de7809ab8df8aa92d94b6c8b98
kkkkendei <authoryu0607@gmail.com>
2025-01-16 17:06:08 +0800

    feat: api注册service

---
+++ api-gateway-center/src/main/java/com/wuzeyu/gateway/center/domain/register/service/RegisterManageService.java
@@ -0,0 +1,36 @@
+package com.wuzeyu.gateway.center.domain.register.service;
+
+import com.wuzeyu.gateway.center.application.IRegisterManageService;
+import com.wuzeyu.gateway.center.domain.register.model.vo.ApplicationInterfaceMethodVO;
+import com.wuzeyu.gateway.center.domain.register.model.vo.ApplicationInterfaceVO;
+import com.wuzeyu.gateway.center.domain.register.model.vo.ApplicationSystemVO;
+import com.wuzeyu.gateway.center.domain.register.repository.IRegisterManageRepository;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wuzeyu
+ * @description api注册服务
+ * @github github.com/kkkkendei
+ */
+public class RegisterManageService implements IRegisterManageService {
+
+    @Resource
+    private IRegisterManageRepository registerManageRepository;
+
+    @Override
+    public void registerApplication(ApplicationSystemVO applicationSystemVO) {
+        registerManageRepository.registerApplication(applicationSystemVO);
+    }
+
+    @Override
+    public void registerApplicationInterface(ApplicationInterfaceVO applicationInterfaceVO) {
+        registerManageRepository.registerApplicationInterface(applicationInterfaceVO);
+    }
+
+    @Override
+    public void registerApplicationInterfaceMethod(ApplicationInterfaceMethodVO applicationInterfaceMethodVO) {
+        registerManageRepository.registerApplicationInterfaceMethod(applicationInterfaceMethodVO);
+    }
+
+}
