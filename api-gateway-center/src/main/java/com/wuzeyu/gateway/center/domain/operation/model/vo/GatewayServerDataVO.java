918758082583f99833a3d89cfa46f6ca5f1b791c
kkkkendei <authoryu0607@gmail.com>
2025-02-03 23:50:09 +0800

    feat: 运营数据查询仓储服务

---
+++ api-gateway-center/src/main/java/com/wuzeyu/gateway/center/domain/operation/model/vo/GatewayServerDataVO.java
@@ -0,0 +1,41 @@
+package com.wuzeyu.gateway.center.domain.operation.model.vo;
+
+/**
+ * @author wuzeyu
+ * @description 网关服务
+ * @github github.com/kkkkendei
+ */
+public class GatewayServerDataVO {
+
+    /** 自增主键 */
+    private Integer id;
+    /** 分组标识 */
+    private String groupId;
+    /** 分组名称 */
+    private String groupName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+}
---
+++ api-gateway-center/src/main/java/com/wuzeyu/gateway/center/domain/operation/repository/IDataOperationManageRepository.java
@@ -0,0 +1,39 @@
+package com.wuzeyu.gateway.center.domain.operation.repository;
+
+import com.wuzeyu.gateway.center.domain.operation.model.vo.*;
+import com.wuzeyu.gateway.center.infrastructure.common.OperationRequest;
+
+import java.util.List;
+
+/**
+ * @author wuzeyu
+ * @description 运营数据查询仓储服务
+ * @github github.com/kkkkendei
+ */
+public interface IDataOperationManageRepository {
+
+    List<GatewayServerDataVO> queryGatewayServerListByPage(OperationRequest<String> request);
+
+    int queryGatewayServerListCountByPage(OperationRequest<String> request);
+
+    List<ApplicationSystemDataVO> queryApplicationSystemListByPage(OperationRequest<ApplicationSystemDataVO> request);
+
+    int queryApplicationSystemListCountByPage(OperationRequest<ApplicationSystemDataVO> request);
+
+    List<ApplicationInterfaceDataVO> queryApplicationInterfaceListByPage(OperationRequest<ApplicationInterfaceDataVO> request);
+
+    int queryApplicationInterfaceListCountByPage(OperationRequest<ApplicationInterfaceDataVO> request);
+
+    List<ApplicationInterfaceMethodDataVO> queryApplicationInterfaceMethodListByPage(OperationRequest<ApplicationInterfaceMethodDataVO> request);
+
+    int queryApplicationInterfaceMethodListCountByPage(OperationRequest<ApplicationInterfaceMethodDataVO> request);
+
+    List<GatewayServerDetailDataVO> queryGatewayServerDetailListByPage(OperationRequest<GatewayServerDetailDataVO> request);
+
+    int queryGatewayServerDetailListCountByPage(OperationRequest<GatewayServerDetailDataVO> request);
+
+    List<GatewayDistributionDataVO> queryGatewayDistributionListByPage(OperationRequest<GatewayDistributionDataVO> request);
+
+    int queryGatewayDistributionListCountByPage(OperationRequest<GatewayDistributionDataVO> request);
+
+}
